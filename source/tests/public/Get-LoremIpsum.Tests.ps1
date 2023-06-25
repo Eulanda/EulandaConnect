c3a5cf92c025c2c924fac0a86c950d3862b5e8a3
Christian Niedergesäß <cn@eulanda.de>
2023-06-25 12:09:04 +0200

    Added Pester-Test for Get-LoremIpsum

---
+++ source/tests/public/Get-LoremIpsum.Tests.ps1
@@ -0,0 +1,27 @@
+Import-Module .\EulandaConnect.psd1
+
+Describe "Get-LoremIpsum Tests" {
+
+    It "Returns at least minimum specified paragraphs" {
+        $minParagraphs = 2
+        $result = Get-LoremIpsum -minParagraphs $minParagraphs
+        $paragraphCount = ([regex]::Matches($result, "`r`n")).Count + 1
+        $paragraphCount | Should -BeGreaterOrEqual $minParagraphs
+    }
+
+    It "Does not exceed the maximum specified paragraphs" {
+        $maxParagraphs = 5
+        $result = Get-LoremIpsum -maxParagraphs $maxParagraphs
+        $paragraphCount = ([regex]::Matches($result, "`r`n")).Count + 1
+        $paragraphCount | Should -BeLessOrEqual $maxParagraphs
+    }
+
+    It "Should return between the specified min and max paragraphs" {
+        $minParagraphs = 3
+        $maxParagraphs = 7
+        $result = Get-LoremIpsum -minParagraphs $minParagraphs -maxParagraphs $maxParagraphs
+        $paragraphCount = ([regex]::Matches($result, "`r`n")).Count + 1
+        $paragraphCount | Should -BeGreaterOrEqual $minParagraphs
+        $paragraphCount | Should -BeLessOrEqual $maxParagraphs
+    }
+}
