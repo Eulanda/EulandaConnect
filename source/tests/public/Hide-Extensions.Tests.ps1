702426fabca2481f67a2b54f17b133d5c575ec47
Christian Niedergesäß <cn@eulanda.de>
2023-06-30 19:29:33 +0200

    Added other pester tests

---
+++ source/tests/public/Hide-Extensions.Tests.ps1
@@ -0,0 +1,23 @@
+Import-Module .\EulandaConnect.psd1
+
+Describe 'Hide-Extensions' {
+    $global:eul1RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $global:eul1RegistryProperty = "HideFileExt"
+    $global:eul1OriginalValue = (Get-ItemProperty -Path $global:eul1RegistryPath).$global:eul1RegistryProperty
+
+    It 'Hide-Extensions changes registry entry' {
+        Hide-Extensions
+        $newValue = (Get-ItemProperty -Path $global:eul1RegistryPath).$global:eul1RegistryProperty
+        $newValue | Should -Be 1
+    }
+
+    AfterAll {
+        # Restore the original value of the registry entry
+        Set-ItemProperty -Path $global:eul1RegistryPath -Name $global:eul1RegistryProperty -Value $global:eul1OriginalValue
+        Update-Desktop
+        # Remove the global variables
+        Remove-Variable -Name eul1RegistryPath -Scope Global
+        Remove-Variable -Name eul1RegistryProperty -Scope Global
+        Remove-Variable -Name eul1OriginalValue -Scope Global
+    }
+}
---
+++ source/tests/public/Show-Extensions.Tests.ps1
@@ -0,0 +1,23 @@
+Import-Module .\EulandaConnect.psd1
+
+Describe 'Show-Extensions' {
+    $global:eul2RegistryPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced"
+    $global:eul2RegistryProperty = "HideFileExt"
+    $global:eul2OriginalValue = (Get-ItemProperty -Path $global:eul2RegistryPath).$global:eul2RegistryProperty
+
+    It 'Show-Extensions changes registry entry' {
+        Show-Extensions
+        $newValue = (Get-ItemProperty -Path $global:eul2RegistryPath).$global:eul2RegistryProperty
+        $newValue | Should -Be 0
+    }
+
+    AfterAll {
+        # Restore the original value of the registry entry
+        Set-ItemProperty -Path $global:eul2RegistryPath -Name $global:eul2RegistryProperty -Value $global:eul2OriginalValue
+        Update-Desktop
+        # Remove the global variables
+        Remove-Variable -Name eul2RegistryPath -Scope Global
+        Remove-Variable -Name eul2RegistryProperty -Scope Global
+        Remove-Variable -Name eul2OriginalValue -Scope Global
+    }
+}
