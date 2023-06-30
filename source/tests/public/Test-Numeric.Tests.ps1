5d215f40076971558981e87831e08484d1737698
Christian Niedergesäß <cn@eulanda.de>
2023-06-30 17:08:11 +0200

    Added other pester tests

---
+++ source/tests/public/Test-Numeric.Tests.ps1
@@ -0,0 +1,37 @@
+Import-Module .\EulandaConnect.psd1
+
+Describe "Test-Numeric" {
+    # Positive Tests
+    It "Returns true for a numeric string" {
+        Test-Numeric -value '12345' | Should -Be $true
+    }
+
+    It "Returns true for a numeric string with decimal point" {
+        Test-Numeric -value '12345.67' | Should -Be $true
+    }
+
+    It "Returns true for a numeric string with zero when allowZero is enabled" {
+        Test-Numeric -value '0' -allowZero | Should -Be $true
+    }
+
+    It "Returns true for a string with numeric and zero when allowZero is enabled" {
+        Test-Numeric -value '123450' -allowZero | Should -Be $true
+    }
+
+    # Negative Tests
+    It "Returns false for a non-numeric string" {
+        Test-Numeric -value 'Hello' | Should -Be $false
+    }
+
+    It "Returns false for an empty string" {
+        Test-Numeric -value '' | Should -Be $false
+    }
+
+    It "Returns false for a numeric string with comma" {
+        Test-Numeric -value '123,45' | Should -Be $false
+    }
+
+    It "Returns false for a numeric string with zero when allowZero is not enabled" {
+        Test-Numeric -value '0' | Should -Be $false
+    }
+}
---
+++ source/tests/public/Test-ReservedIp.Tests.ps1
@@ -0,0 +1,22 @@
+Import-Module .\EulandaConnect.psd1
+
+Describe "Test-ReservedIp" {
+    # Positive Tests
+    It "Returns true for a reserved IP"  {
+        Test-ReservedIp -ip '127.0.0.1' | Should -Be $true
+    }
+
+    It "Returns false for a non-reserved IP"  {
+        Test-ReservedIp -ip '8.8.8.8' | Should -Be $false
+    }
+
+    # Negative Tests
+    It "Throws an exception for an invalid IP"  {
+        { Test-ReservedIp -ip '300.300.300.300' } | Should -Throw
+    }
+
+    It "Throws an exception for a non-IP string"  {
+        { Test-ReservedIp -ip 'Hello World' } | Should -Throw
+    }
+
+}
