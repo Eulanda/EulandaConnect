18e8736e8c2cf5ebeca9ba55d9b87de6ac2a82dd
Christian Niedergesäß <cn@eulanda.de>
2023-07-01 07:19:06 +0200

    Added other pester tests

---
+++ source/tests/private/Convert-DatanormDateFormat.Tests.ps1
@@ -0,0 +1,28 @@
+# Import the module that contains the function
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Convert-DatanormDateFormat' {
+
+    InModuleScope 'EulandaConnect' {
+
+        It 'converts Datanorm date format to ISO 8601 format correctly' {
+            # Arrange
+            $datanormDate = '310122'
+            $expectedISODate = '2022-01-31'
+
+            # Act
+            $actualISODate = Convert-DatanormDateFormat -date $datanormDate
+
+            # Assert
+            $actualISODate | Should -Be $expectedISODate
+        }
+
+        It 'throws an exception for an invalid Datanorm date format' {
+            # Arrange
+            $invalidDatanormDate = 'invalid-date'
+
+            # Act and Assert
+            { Convert-DatanormDateFormat -date $invalidDatanormDate } | Should -Throw
+        }
+    }
+}
