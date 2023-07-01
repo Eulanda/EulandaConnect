f1b576879e005fd6435181c239df7f31d27199ca
Christian Niedergesäß <cn@eulanda.de>
2023-07-01 22:28:50 +0200

    Modified pester test for New-FtpFolder

--- source/tests/private/New-FtpFolder.Tests.ps1
+++ source/tests/private/New-FtpFolder.Tests.ps1
@@ -14,20 +14,30 @@ Describe "New-FtpFolder" -Tag 'Integration' {
             $secure = Import-Clixml -path $path
             $server = $ini['SFTP']['Server']
             $user = $ini['SFTP']['User']
+        }
+
+        It "Creates a new folder and checks if it is listed in the remote directory" {
 
             # Generate a random string of 10 characters for the folder name
             $folderName = -join ((65..90) | Get-Random -Count 10 | ForEach-Object {[char]$_})
 
-            # Act
+            # Create a random folder
             New-FtpFolder -server $server -user $user -password $secure -remoteFolder "/$folderName"
-        }
 
-        It "Creates a new folder and checks if it is listed in the remote directory" {
-            # Assert
-
-            # Check if the new folder is in the remote directory
+            # Check if the file exists
             $result = Get-FtpDir -server $server -user $user -password $secure -dirType directory
             $result | Should -Contain $folderName
+
+            # Remove the new folder
+            Remove-FtpFolder -server $server -user $user -password $secure -remoteFolder "/$folderName"
+
+            # Re-Read the directory for folder
+            $result = Get-FtpDir -server $server -user $user -password $secure -dirType directory
+
+            # The file should no longer exist
+            if ($result) {
+                $result.Contains($folderName) | Should -Be $false
+            }
         }
     }
 }
