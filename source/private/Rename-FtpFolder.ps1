30e857eb8b7f26345dbf51836b09bb7f060d00f2
Christian Niedergesäß <cn@eulanda.de>
2023-07-02 09:25:53 +0200

    Added pester test for Rename-FtpFolder

--- source/private/Rename-FtpFolder.ps1
+++ source/private/Rename-FtpFolder.ps1
@@ -99,5 +99,22 @@ function Rename-FtpFolder {
     end {
         Get-CurrentVariables -InitialVariables $initialVariables -Debug:$DebugPreference
     }
-    # Test:  Rename-FtpFolder -server 'myftp.eulanda.eu' -user 'johndoe' -password 'secure' -remoteFolder '/EULANDA' newFolder '/BELANDA'
+
+    <#
+
+        $Features = Import-Module -Name '.\EulandaConnect.psm1' -PassThru -Force
+        & $Features {
+            $pesterFolder = Resolve-Path -path ".\source\tests"
+            $iniPath = Join-Path -path $pesterFolder "pester.ini"
+            $ini = Read-IniFile -path $iniPath
+            $path = $ini['SFTP']['SecurePasswordPath']
+            $path = $path -replace '\$home', $HOME
+            $secure = Import-Clixml -path $path
+            $server = $ini['SFTP']['Server']
+            $user = $ini['SFTP']['User']
+            Rename-FtpFolder -server $server -user $user -password $secure -remoteFolder '/inbox' -newFolder '/inbox-new'
+        }
+
+        # The 'inbox' folder example belongs to the ftp server test environment we recommend.
+    #>
 }
---
+++ source/tests/private/Rename-FtpFolder.Tests.ps1
@@ -0,0 +1,57 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+# ATTENTION: This integration test requires MSSQL, FTP or something other
+
+Describe 'Test-FtpFolder' -Tag 'integration', 'ftp' {
+    InModuleScope 'EulandaConnect' {
+
+        BeforeAll {
+            $pesterFolder = Resolve-Path -path ".\source\tests"
+            $iniPath = Join-Path -path $pesterFolder "pester.ini"
+            $ini = Read-IniFile -path $iniPath
+            $path = $ini['SFTP']['SecurePasswordPath']
+            $path = $path -replace '\$home', $HOME
+            $secure = Import-Clixml -path $path
+            $server = $ini['SFTP']['Server']
+            $user = $ini['SFTP']['User']
+
+            $folderOld = '/inbox'
+            $folderNew = '/inbox-new'
+        }
+
+        It "Verifies if the old FTP folder exists at the beginning" {
+            $result = Test-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderOld
+            $result | Should -Be $true
+        }
+
+
+        It "Verifies if the new FTP folder does not exists at beginning" {
+            $result = Test-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderNew
+            $result | Should -Not -Be $true
+        }
+
+        It "Verifies if renaming the folder works without exception" {
+            {
+                Rename-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderOld -newFolder $folderNew
+            } | Should -Not -Throw
+        }
+
+
+        It "Verifies if renamed folder exists" {
+            $result = Test-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderNew
+            $result | Should -Be $true
+        }
+
+
+        It "Verifies if renaming back the folder works without exception" {
+            {
+                Rename-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderNew -newFolder $folderOld
+            } | Should -Not -Throw
+        }
+
+        It "Verifies if the FTP folder exists like at beginning" {
+            $result = Test-FtpFolder -server $server -user $user -password $secure -remoteFolder $folderOld
+            $result | Should -Be $true
+        }
+    }
+}
