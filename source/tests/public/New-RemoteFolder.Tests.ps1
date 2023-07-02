7de2e99273bfad0f6f5aabbc75f4b6b3d752aa14
Christian Niedergesäß <cn@eulanda.de>
2023-07-02 15:21:54 +0200

    Added pester test for ReceiveRemoteFile

--- source/tests/public/New-RemoteFolder.Tests.ps1
+++ source/tests/public/New-RemoteFolder.Tests.ps1
@@ -3,73 +3,72 @@ Import-Module -Name .\EulandaConnect.psd1
 # ATTENTION: This integration test requires MSSQL, FTP or something other
 
 Describe 'New-RemoteFolder' -Tag 'integration', 'ftp', 'sftp' {
-    InModuleScope 'EulandaConnect' {
 
-        BeforeAll {
-            # Arrange
-            $pesterFolder = Resolve-Path -path ".\source\tests"
-            $iniPath = Join-Path -path $pesterFolder "pester.ini"
-            $ini = Read-IniFile -path $iniPath
-            $path = $ini['SFTP']['SecurePasswordPath']
-            $path = $path -replace '\$home', $HOME
-            $secure = Import-Clixml -path $path
-            $server = $ini['SFTP']['Server']
-            $user = $ini['SFTP']['User']
-        }
+    BeforeAll {
+        # Arrange
+        $pesterFolder = Resolve-Path -path ".\source\tests"
+        $iniPath = Join-Path -path $pesterFolder "pester.ini"
+        $ini = Read-IniFile -path $iniPath
+        $path = $ini['SFTP']['SecurePasswordPath']
+        $path = $path -replace '\$home', $HOME
+        $secure = Import-Clixml -path $path
+        $server = $ini['SFTP']['Server']
+        $user = $ini['SFTP']['User']
+    }
 
-        # **********
-        # SFTP calls
-        # **********
-        It "Creates a new folder and checks if it is listed in the remote directory via sftp" {
+    # **********
+    # SFTP calls
+    # **********
+    It "Creates a new folder and checks if it is listed in the remote directory via sftp" {
 
-            # Generate a random string of 10 characters for the folder name
-            $folderName = -join ((65..90) | Get-Random -Count 10 | ForEach-Object {[char]$_})
+        # Generate a random string of 10 characters for the folder name
+        $folderName = -join ((65..90) | Get-Random -Count 10 | ForEach-Object {[char]$_})
 
-            # Create a random folder
-            New-RemoteFolder -server $server -protocol sftp -user $user -password $secure -remoteFolder "/$folderName"
+        # Create a random folder
+        New-RemoteFolder -server $server -protocol sftp -user $user -password $secure -remoteFolder "/$folderName"
 
-            # Check if the file exists
-            $result = Get-RemoteDir -server $server -protocol sftp -user $user -password $secure -dirType directory
-            $result | Should -Contain $folderName
+        # Check if the file exists
+        $result = Get-RemoteDir -server $server -protocol sftp -user $user -password $secure -dirType directory
+        $result | Should -Contain $folderName
 
-            # Remove the new folder
-            Remove-RemoteFolder -server $server -protocol sftp -user $user -password $secure -remoteFolder "/$folderName"
+        # Remove the new folder
+        Remove-RemoteFolder -server $server -protocol sftp -user $user -password $secure -remoteFolder "/$folderName"
 
-            # Re-Read the directory for folder
-            $result = Get-RemoteDir -server $server -protocol sftp -user $user -password $secure -dirType directory
+        # Re-Read the directory for folder
+        $result = Get-RemoteDir -server $server -protocol sftp -user $user -password $secure -dirType directory
 
-            # The file should no longer exist
-            if ($result) {
-                $result.Contains($folderName) | Should -Be $false
-            }
-        }
-
-
-        # **********
-        # FTP calls
-        # **********
-        It "Creates a new folder and checks if it is listed in the remote directory via ftp" {
-
-            # Generate a random string of 10 characters for the folder name
-            $folderName = -join ((65..90) | Get-Random -Count 10 | ForEach-Object {[char]$_})
-
-            # Create a random folder
-            New-RemoteFolder -server $server -protocol ftp -user $user -password $secure -remoteFolder "/$folderName"
-
-            # Check if the file exists
-            $result = Get-RemoteDir -server $server -protocol ftp -user $user -password $secure -dirType directory
-            $result | Should -Contain $folderName
-
-            # Remove the new folder
-            Remove-RemoteFolder -server $server -protocol ftp -user $user -password $secure -remoteFolder "/$folderName"
-
-            # Re-Read the directory for folder
-            $result = Get-RemoteDir -server $server -protocol ftp -user $user -password $secure -dirType directory
-
-            # The file should no longer exist
-            if ($result) {
-                $result.Contains($folderName) | Should -Be $false
-            }
+        # The file should no longer exist
+        if ($result) {
+            $result.Contains($folderName) | Should -Be $false
         }
     }
+
+
+    # **********
+    # FTP calls
+    # **********
+    It "Creates a new folder and checks if it is listed in the remote directory via ftp" {
+
+        # Generate a random string of 10 characters for the folder name
+        $folderName = -join ((65..90) | Get-Random -Count 10 | ForEach-Object {[char]$_})
+
+        # Create a random folder
+        New-RemoteFolder -server $server -protocol ftp -user $user -password $secure -remoteFolder "/$folderName"
+
+        # Check if the file exists
+        $result = Get-RemoteDir -server $server -protocol ftp -user $user -password $secure -dirType directory
+        $result | Should -Contain $folderName
+
+        # Remove the new folder
+        Remove-RemoteFolder -server $server -protocol ftp -user $user -password $secure -remoteFolder "/$folderName"
+
+        # Re-Read the directory for folder
+        $result = Get-RemoteDir -server $server -protocol ftp -user $user -password $secure -dirType directory
+
+        # The file should no longer exist
+        if ($result) {
+            $result.Contains($folderName) | Should -Be $false
+        }
+    }
+
 }
---
+++ source/tests/public/Receive-RemoteFile.Tests.ps1
@@ -0,0 +1,58 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+# ATTENTION: This integration test requires MSSQL, FTP or something other
+
+Describe 'Receive-RemoteFile' -Tag 'integration', 'ftp', 'sftp' {
+
+    BeforeAll {
+        # Arrange
+        $pesterFolder = Resolve-Path -path ".\source\tests"
+        $iniPath = Join-Path -path $pesterFolder "pester.ini"
+        $ini = Read-IniFile -path $iniPath
+        $path = $ini['SFTP']['SecurePasswordPath']
+        $path = $path -replace '\$home', $HOME
+        $secure = Import-Clixml -path $path
+        $server = $ini['SFTP']['Server']
+        $user = $ini['SFTP']['User']
+    }
+
+    # **********
+    # SFTP calls
+    # **********
+    It "Downloads a file, checks its contents, and deletes the file via sftp" {
+        # Download the file
+        $localFile = Join-Path $env:TEMP 'license.md'
+        Receive-RemoteFile -server $server -protocol sftp -user $user -password $secure -remoteFile 'License.md' -localFolder $env:TEMP
+
+        # Check that the file exists
+        $localFile | Should -Exist
+
+        # Check the file contents
+        $content = Get-Content -Path $localFile -Raw
+        $content | Should -Match 'CONNECTION WITH THE SOFTWARE'
+
+        # Clean up the downloaded file
+        Remove-Item -Path $localFile -Force
+        $localFile | Should -Not -Exist
+    }
+
+    # **********
+    # FTP calls
+    # **********
+    It "Downloads a file, checks its contents, and deletes the file via ftp" {
+        # Download the file
+        $localFile = Join-Path $env:TEMP 'license.md'
+        Receive-RemoteFile -server $server -protocol ftp -user $user -password $secure -remoteFile 'License.md' -localFolder $env:TEMP
+
+        # Check that the file exists
+        $localFile | Should -Exist
+
+        # Check the file contents
+        $content = Get-Content -Path $localFile -Raw
+        $content | Should -Match 'CONNECTION WITH THE SOFTWARE'
+
+        # Clean up the downloaded file
+        Remove-Item -Path $localFile -Force
+        $localFile | Should -Not -Exist
+    }
+}
