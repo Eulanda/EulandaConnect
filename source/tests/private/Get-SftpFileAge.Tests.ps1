ccfb37c8621262edbbde7547a012d406a22da1e6
Christian Niedergesäß <cn@eulanda.de>
2023-07-02 10:55:13 +0200

    Added pester test for Get-SftpFileAge

---
+++ source/tests/private/Get-SftpFileAge.Tests.ps1
@@ -0,0 +1,40 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+# ATTENTION: This integration test requires MSSQL, FTP or something other
+
+Describe 'Get-SftpFileAge' -Tag 'integration', 'sftp' {
+    InModuleScope 'EulandaConnect' {
+
+        BeforeAll {
+            # Arrange
+            $pesterFolder = Resolve-Path -path ".\source\tests"
+            $iniPath = Join-Path -path $pesterFolder "pester.ini"
+            $ini = Read-IniFile -path $iniPath
+            $path = $ini['SFTP']['SecurePasswordPath']
+            $path = $path -replace '\$home', $HOME
+            $secure = Import-Clixml -path $path
+            $server = $ini['SFTP']['Server']
+            $user = $ini['SFTP']['User']
+
+            # Calculate the acceptable range
+            $now = Get-Date
+            $earliest = [DateTime]::new(2023, 1, 1)
+            $latest = $now.AddMinutes(-10)
+
+            $earliestSeconds = ($now - $earliest).TotalSeconds
+            $latestSeconds = ($now - $latest).TotalSeconds
+
+            # Act
+            $result = Get-SftpFileAge -server $server -user $user -password $secure -remoteFile 'License.md'
+        }
+
+        It "Gets the age of the file 'License.md' in seconds and checks if it's within an acceptable range" {
+
+            # Check if the age of the file is less than the difference in seconds since 01.01.2023
+            $result | Should -BeLessOrEqual $earliestSeconds
+
+            # Check if the age of the file is greater than 10 minutes
+            $result | Should -BeGreaterOrEqual $latestSeconds
+        }
+    }
+}
