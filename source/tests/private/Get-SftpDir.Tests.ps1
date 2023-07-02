6a4a1efead7ef0c8370cc1eaf16582c56d294a08
Christian Niedergesäß <cn@eulanda.de>
2023-07-02 10:49:39 +0200

    Sourcecode refactoring

---
+++ source/tests/private/Get-SftpDir.Tests.ps1
@@ -0,0 +1,37 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+# ATTENTION: This integration test requires MSSQL, FTP or something other
+
+Describe 'Get-SftpDir' -Tag 'integration', 'sftp' {
+    InModuleScope 'EulandaConnect' {
+
+        BeforeAll {
+            $pesterFolder = Resolve-Path -Path ".\source\tests"
+            $iniPath = Join-Path -Path $pesterFolder "pester.ini"
+            $ini = Read-IniFile -Path $iniPath
+            $path = $ini['SFTP']['SecurePasswordPath']
+            $path = $path -replace '\$home', $HOME
+            $secure = Import-Clixml -Path $path
+            $server = $ini['SFTP']['Server']
+            $user = $ini['SFTP']['User']
+
+            $expectedOutput = @('inbox', 'outbox') # Update this to reflect the actual output
+        }
+
+        It "returns the expected root directory list from SFTP server" {
+            # Act
+            $result = Get-SftpDir -server $server -user $user -password $secure -remoteFolder '/' -dirType directory
+
+            # Assert
+            $result | Should -Be $expectedOutput
+        }
+
+        It "returns directory (default root) list from SFTP server" {
+            # Act
+            $result = Get-SftpDir -server $server -user $user -password $secure -dirType directory
+
+            # Assert
+            $result | Should -Be $expectedOutput
+        }
+    }
+}
