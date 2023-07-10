7f6dca4d14e161b369bee3e861931d69e2fb984a
Christian Niedergesaess <cn@DadoGo.eulanda.eu>
2023-07-10 09:27:02 +0200

    Convert-FromDatanorm pester test optimized

--- source/tests/public/Convert-FromDatanorm.Tests.ps1
+++ source/tests/public/Convert-FromDatanorm.Tests.ps1
@@ -1,10 +1,16 @@
 Import-Module -Name .\EulandaConnect.psd1
 
 Describe "Convert-FromDatanorm" {
-    It "should convert the Datanorm file to objects" {
-        # Arrange
 
-        $data = @(
+    BeforeAll {
+        $randomName = [System.IO.Path]::GetRandomFileName()
+        $randomName = $randomName -replace '\.',''
+
+        $tempFolder = Join-Path -Path $env:TEMP -ChildPath $randomName
+        New-Item -ItemType Directory -Path $tempFolder | Out-Null
+        $tempFile = Join-Path -Path $tempFolder -ChildPath "DATANORM.001"
+
+        $dataRecords = @(
             "V 120623J.W.Zander GmbH & Co.KG                 E-Business                              Hr.Gitzinger Tel.: 0761-5140219    04EUR",
             "A;A;8241335;00;ELTROPA Verdrahtungsbrcke 1ph 265mm sw;EVB 10/265 A2 10qmm Stift isol;1;0;Stk;257;EM01;01;;",
             "B;A;8241335;EVB 10/265 A2;456 01 31;;1;150;250;4003899170225;;010204;0;1;;;",
@@ -19,12 +25,12 @@ Describe "Convert-FromDatanorm" {
             "P;A;070175003;2;1974;;;;;;;070508001;1;0;0; ;;;;;070508001;2;0;0; ;;;;;",
             ""
         )
-        $randomName = [System.IO.Path]::GetRandomFileName()
-        $randomName = $randomName -replace '\.',''
+    }
 
-        $tempFolder = Join-Path -Path $env:TEMP -ChildPath $randomName
-        New-Item -ItemType Directory -Path $tempFolder | Out-Null
-        $tempFile = Join-Path -Path $tempFolder -ChildPath "DATANORM.001"
+
+    It "should convert the Datanorm file to objects" {
+        # Arrange
+        $data = $dataRecords
         $data | Out-File -FilePath $tempFile -Encoding ASCII
 
         # Act
@@ -32,23 +38,26 @@ Describe "Convert-FromDatanorm" {
 
         # Assert
         $result | Should -BeOfType [System.Management.Automation.PSCustomObject]
+    }
 
-        # Assert the specific object
+
+    It "should convert the Datanorm file to expected article number" {
+        # Arrange
+        $data = $dataRecords
+        $data | Out-File -FilePath $tempFile -Encoding ASCII
+
+        # Act
+        $result = Convert-FromDatanorm -Path $tempFile
+
+        # Assert
         $specificObject = $result.a["8241335"]
         $specificObject | Should -Not -BeNull
         $specificObject.ArtikelNummer | Should -Be "8241335"
-
-        Remove-Item -Path $tempFolder -Force -Recurse
     }
+
 
     It "should handle empty files gracefully" {
         # Arrange
-        $randomName = [System.IO.Path]::GetRandomFileName()
-        $randomName = $randomName -replace '\.',''
-
-        $tempFolder = Join-Path -Path $env:TEMP -ChildPath $randomName
-        New-Item -ItemType Directory -Path $tempFolder | Out-Null
-        $tempFile = Join-Path -Path $tempFolder -ChildPath "DATANORM.001"
         $data = ""
         $data | Out-File -FilePath $tempFile -Encoding ASCII
 
@@ -57,7 +66,11 @@ Describe "Convert-FromDatanorm" {
 
         # Assert
         $result | Should -BeNullOrEmpty
+    }
 
+
+    AfterAll {
+        # Cleanup
         Remove-Item -Path $tempFolder -Force -Recurse
     }
 }
