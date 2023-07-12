52a8c2417b07ba519317f99dcbb19c7780832a3b
Christian Niedergesaess <cn@eulanda.de>
2023-07-12 20:38:10 +0200

    Added pester test for Get-Filename

---
+++ source/tests/public/Get-Filename.Tests.ps1
@@ -0,0 +1,21 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-Filename' {
+    It 'returns the correct filename from a valid path' {
+        $filePath = Join-Path -Path 'C:\temp' -ChildPath 'test.txt'
+        Get-Filename -path $filePath | Should -Be 'test.txt'
+    }
+
+    It 'throws an exception if the path ends with a period' {
+        { Get-Filename -path 'C:\temp\test.' } | Should -Throw '*A valid filepath cannot end with a period.*'
+    }
+
+    It 'throws an exception if the path ends with a backslash' {
+        { Get-Filename -path 'C:\temp\' } | Should -Throw '*A valid filepath cannot end with a backslash.*'
+    }
+
+    It 'throws an exception if the path ends with a blank character' {
+        { Get-Filename -path 'C:\temp\test ' } | Should -Throw '*A valid filepath cannot end with a blank character.*'
+    }
+}
+
