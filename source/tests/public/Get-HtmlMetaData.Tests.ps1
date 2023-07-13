55898473c0bc51be9f03479ff66810521526a449
Christian Niedergesaess <cn@eulanda.de>
2023-07-13 14:00:13 +0200

    Added pester test for Get-HtmlMetaData

---
+++ source/tests/public/Get-HtmlMetaData.Tests.ps1
@@ -0,0 +1,19 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-HtmlMetaData' {
+
+    It 'should return a hashtable with default values when no parameters are provided' {
+        $metaData = Get-HtmlMetaData
+        $metaData.author | Should -Be "EULANDA Software GmbH - ERP-Systems - Germany"
+        $metaData.generator | Should -Be "Powershell $($PsVersiontable.GitCommitId) by function ConvertTo-Html"
+        $metaData.keywords | Should -Be "eulanda, erp, powershell, convertto-html, html"
+        $metaData.viewport | Should -Be "width=device-width, initial-scale=1.0"
+        $metaData.ContainsKey('description') | Should -Be $false
+    }
+
+    It 'should include a description when provided as a parameter' {
+        $description = 'My meta description'
+        $metaData = Get-HtmlMetaData -description $description
+        $metaData.description | Should -Be $description
+    }
+}
