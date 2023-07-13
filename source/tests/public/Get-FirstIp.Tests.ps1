6d4941196e199a8563d65bf1d9dd4e2872b46bd0
Christian Niedergesaess <cn@eulanda.de>
2023-07-13 13:09:20 +0200

    Added pester test for Get-FirstIp

---
+++ source/tests/public/Get-FirstIp.Tests.ps1
@@ -0,0 +1,18 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-FirstIp' {
+
+    It 'Get-FirstIp should return the next IP for the subnet 255.255.255.0' {
+        Get-FirstIp -cidr 24 -NetworkId '192.168.1.0' | Should -Be '192.168.1.1'
+    }
+
+    It 'Get-FirstIp should return the next IP for a different subnet' {
+        Get-FirstIp -cidr 16 -NetworkId '192.168.2.0' | Should -Be '192.168.2.1'
+    }
+
+    It 'Get-FirstIp should return the first IP in a new subnet' {
+        Get-FirstIp -cidr 8 -NetworkId '192.168.3.0' | Should -Be '192.168.3.1'
+    }
+
+}
+
