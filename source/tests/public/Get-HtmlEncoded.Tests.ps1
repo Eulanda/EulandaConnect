cb273632a764637793efbeac7055eb45fdafa1d6
Christian Niedergesaess <cn@eulanda.de>
2023-07-13 13:53:10 +0200

    Added pester test for Get-HtmlEncoded

---
+++ source/tests/public/Get-HtmlEncoded.Tests.ps1
@@ -0,0 +1,24 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-HtmlEncoded' {
+
+    It 'should return untagged strings unchanged' {
+        $string = 'This is a string without any tags.'
+        $encodedString = Get-HtmlEncoded -taggedString $string
+        $encodedString | Should -Be $string
+    }
+
+    It 'should correctly encode and restore recognized tags' {
+        $string = 'This is <b>bold</b> and this is <strong>strong</strong>.'
+        $expectedResult = 'This is <b>bold</b> and this is <strong>strong</strong>.'
+        $encodedString = Get-HtmlEncoded -taggedString $string
+        $encodedString | Should -Be $expectedResult
+    }
+
+    It 'should replace unrecognized tags with HTML encoded versions' {
+        $string = 'This is <tag>not recognized</tag>.'
+        $expectedResult = 'This is &lt;tag&gt;not recognized&lt;/tag&gt;.'
+        $encodedString = Get-HtmlEncoded -taggedString $string
+        $encodedString | Should -Be $expectedResult
+    }
+}
