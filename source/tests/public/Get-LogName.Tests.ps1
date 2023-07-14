55a5302890d1d3d4efd30bb19648d443d7ed0e68
Christian Niedergesaess <cn@eulanda.de>
2023-07-14 20:17:19 +0200

    Added some Pester Tests

---
+++ source/tests/public/Get-LogName.Tests.ps1
@@ -0,0 +1,22 @@
+
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-LogName' {
+
+    It 'Should return a log name with default ident when no ident is provided' {
+        $logName = Get-LogName
+        $logName | Should -Match "LOG-DEF-\d{4}-\d{2}-\d{2}--\d{2}-\d{2}-\d{2}-\d{3}.txt"
+    }
+
+
+    It 'Should include the ident in the log name when an ident is provided' {
+        $logName = Get-LogName -ident 'STANDARD'
+        $logName | Should -Match "LOG-STANDARD-\d{4}-\d{2}-\d{2}--\d{2}-\d{2}-\d{2}-\d{3}.txt"
+    }
+
+
+    It 'Should format the log name according to the dateMask when a dateMask is provided' {
+        $logName = Get-LogName -ident 'STANDARD' -dateMask 'yyyy-MM'
+        $logName | Should -Match "LOG-STANDARD-\d{4}-\d{2}.txt"
+    }
+}
---
+++ source/tests/public/Get-MaxHosts.Tests.ps1
@@ -0,0 +1,27 @@
+# Importieren Sie das Modul, falls es noch nicht importiert wurde
+# Import-Module -Name 'EulandaConnect'
+
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-MaxHosts' {
+
+    # Test if no subnet or CIDR values are provided, the function should retrieve the
+    # subnet from the local IP and calculate the number of hosts
+    It 'Should retrieve subnet from local IP and calculate the number of hosts when no subnet or CIDR is provided' {
+        $hosts = Get-MaxHosts
+        $hosts | Should -BeGreaterThan 0
+    }
+
+    # Test, if a CIDR value is provided, the function should calculate the number of hosts accordingly
+    It 'Should calculate the number of hosts according to the provided CIDR value' {
+        $hosts = Get-MaxHosts -cidr 30
+        $hosts | Should -Be 2
+    }
+
+    # Tests if an invalid CIDR value is provided, the function should return a neg. value
+    It 'Should return a negative value when an invalid CIDR value is provided' {
+        $hosts = Get-MaxHosts -cidr 33
+        $hosts | Should -BeLessThan 0
+    }
+
+}
