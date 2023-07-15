b43b67d6ca4f6ebdc10a1f4b11bb838f1a183f83
Christian Niedergesaess <cn@eulanda.de>
2023-07-15 17:59:29 +0200

    Added pester test for Get-PublicIp

---
+++ source/tests/public/Get-PublicIp.Tests.ps1
@@ -0,0 +1,11 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-PublicIp" {
+    It "Returns a valid public IP address" {
+        # Invoke function
+        $result = Get-PublicIp
+
+        # Check that the result is a valid IP address
+        $result | Should -Match '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+    }
+}
