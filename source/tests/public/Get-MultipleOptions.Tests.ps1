4ed54b473f2981140f7a125a4b05e8bff1f6fd58
Christian Niedergesaess <cn@eulanda.de>
2023-07-15 17:03:24 +0200

    Added pester test for Get-MultipleOptions

---
+++ source/tests/public/Get-MultipleOptions.Tests.ps1
@@ -0,0 +1,23 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-MultipleOptions" {
+
+    It "Runs without throwing" {
+        { Get-MultipleOptions -values 'alpha,bravo,charlie' -list @('default','bravo','charlie','delta','echo') } | Should -Not -Throw
+    }
+
+    It "Returns correct output when values are in list" {
+        $result = Get-MultipleOptions -values 'alpha,bravo,charlie' -list @('default','bravo','charlie','delta','echo')
+        $result | Should -Be 'default,bravo,charlie'
+    }
+
+    It "Returns first item from list when no values provided" {
+        $result = Get-MultipleOptions -values '' -list @('default','bravo','charlie','delta','echo')
+        $result | Should -Be 'default'
+    }
+
+    It "Returns first item from list when values are not in list" {
+        $result = Get-MultipleOptions -values 'foxtrot,golf,horse' -list @('default','bravo','charlie','delta','echo')
+        $result | Should -Be 'default'
+    }
+}
