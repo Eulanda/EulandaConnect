87bb65b248164b0e9e042ac8e9b82735150581a9
Christian Niedergesaess <cn@eulanda.de>
2023-07-15 18:05:08 +0200

    Added pester test for Get-ScriptDir

---
+++ source/tests/public/Get-ScriptDir.Tests.ps1
@@ -0,0 +1,12 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-ScriptDir" {
+    It "Returns a valid script directory" {
+        # Invoke function
+        $result = Get-ScriptDir
+
+        # Check that the result is an existing directory
+        $result | Should -Exist
+        Test-Path -Path $result -PathType Container | Should -Be $true
+    }
+}
