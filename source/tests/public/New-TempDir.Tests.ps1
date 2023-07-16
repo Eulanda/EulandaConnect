edc80d069e02790479d513c59b169d92cdc8368d
Christian Niedergesaess <cn@eulanda.de>
2023-07-16 12:18:58 +0200

    Added pester test for New-TempDir

---
+++ source/tests/public/New-TempDir.Tests.ps1
@@ -0,0 +1,15 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'New-TempDir' {
+    It 'Creates a new temp directory' {
+        $tempDir = New-TempDir
+        Test-Path -Path $tempDir | Should -BeTrue
+    }
+
+    AfterEach {
+        # Cleanup the created temp directory
+        if (Test-Path -Path $tempDir) {
+            Remove-Item -Path $tempDir -Recurse -Force
+        }
+    }
+}
