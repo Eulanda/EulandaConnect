3fa39ff53870875d8774f102e2a7438afcc14fc1
Christian Niedergesaess <cn@eulanda.de>
2023-07-16 16:53:01 +0200

    Added pester test for Get-BroadcastIp

---
+++ source/tests/public/Get-BroadcastIp.Tests.ps1
@@ -0,0 +1,16 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-BroadcastIp" {
+    It "Runs without throwing" {
+        { Get-LastIp } | Should -Not -Throw
+    }
+
+    It "Returns the correct IP when networkId and cidr are specified" {
+        $lastIp = Get-BroadcastIp -networkId "192.168.172.8" -cidr 30
+        $lastIp | Should -Be "192.168.172.11"
+    }
+
+    It "Doesn't return an empty string" {
+        Get-BroadcastIp | Should -Not -BeNullOrEmpty
+    }
+}
