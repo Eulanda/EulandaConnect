1f91a047d4b2492cd84d4824819041a1ed103ebb
Christian Niedergesaess <cn@eulanda.de>
2023-07-16 16:23:52 +0200

    Added pester test for Get-LastIp

---
+++ source/tests/public/Get-LastIp.Tests.ps1
@@ -0,0 +1,16 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-LastIp" {
+    It "Runs without throwing" {
+        { Get-LastIp } | Should -Not -Throw
+    }
+
+    It "Returns the correct IP when networkId and cidr are specified" {
+        $lastIp = Get-LastIp -networkId "192.168.172.8" -cidr 30
+        $lastIp | Should -Be "192.168.172.10"
+    }
+
+    It "Doesn't return an empty string" {
+        Get-LastIp | Should -Not -BeNullOrEmpty
+    }
+}
