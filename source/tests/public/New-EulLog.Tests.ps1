40ccbab274a4048e7668b0b5ce15a52f8576ee25
Christian Niedergesaess <cn@eulanda.de>
2023-07-16 11:31:16 +0200

    Addd pester test for New-EulLog

---
+++ source/tests/public/New-EulLog.Tests.ps1
@@ -0,0 +1,30 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'New-EulLog' {
+    BeforeAll {
+        $global:TestPath = "Path\To\Test"
+        $global:TestName = "TestLog"
+    }
+
+    It 'New-EulLog creates EulLog' {
+        $eulLog = New-EulLog -name $global:TestName -path $global:TestPath
+        $eulLog | Should -Not -BeNullOrEmpty
+        $eulLog.filePath | Should -Be "$global:TestPath\events\LOG_$global:TestName`_$(Get-Date -Uformat '%Y-%b').txt"
+        $eulLog.name | Should -Be $global:TestName
+    }
+
+    It 'EulLog.Put logs message' {
+        $eulLog = New-EulLog -name $global:TestName -path $global:TestPath
+        $message = 'Test message'
+        $eulLog.Put($message)
+        $logContent = Get-Content -Path $eulLog.filePath
+        $logContent.Where({ $_ -like "*$message*" }) | Should -Not -BeNullOrEmpty
+    }
+
+
+
+    # Cleanup after tests
+    AfterAll {
+        Remove-Item -Path "$global:TestPath\events\LOG_$global:TestName`_*.txt" -Force -ErrorAction SilentlyContinue
+    }
+}
