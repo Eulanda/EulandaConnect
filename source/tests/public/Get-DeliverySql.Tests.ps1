5b2760a64f543cc5e48c4861fb623097d7bbaf6f
Christian Niedergesaess <cn@eulanda.de>
2023-07-17 15:14:20 +0200

    Added pester test for Get-DeliverySql

---
+++ source/tests/public/Get-DeliverySql.Tests.ps1
@@ -0,0 +1,26 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-DeliverySql" {
+    Context "With deliveryId and deliveryNo parameters" {
+        It "Should return an array of SQL queries" {
+            # Arrange
+            $deliveryNo = 5678
+
+            # Act
+            $result = Get-DeliverySql -deliveryNo $deliveryNo
+
+            # Assert
+            $result | Should -Not -BeNullOrEmpty
+            $result.Count | Should -Be 2
+            $result[0] | Should -Match "SELECT"
+            $result[1] | Should -Match "SELECT"
+        }
+    }
+
+    Context "Without parameters" {
+        It "Should throw an error" {
+            { Get-DeliverySql } | Should -Throw
+        }
+    }
+
+}
