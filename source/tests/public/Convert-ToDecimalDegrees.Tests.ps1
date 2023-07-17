b5f845e681a93ad4905cbd7a683908cbe86124ff
Christian Niedergesaess <cn@eulanda.de>
2023-07-17 15:31:26 +0200

    Added pester test for Convert-ToDecimalDegrees

---
+++ source/tests/public/Convert-ToDecimalDegrees.Tests.ps1
@@ -0,0 +1,33 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Convert-ToDecimalDegrees" {
+    It "Converts degrees, minutes, and seconds to decimal format" {
+        # Arrange
+        $degrees = 40
+        $minutes = 20
+        $seconds = 30
+        $direction = 'N'
+
+        # Act
+        $result = Convert-ToDecimalDegrees -degrees $degrees -minutes $minutes -seconds $seconds -direction $direction
+
+        # Assert
+        $expectedResult = $degrees + ($minutes / 60) + ($seconds / 3600)
+        $result | Should -Be $expectedResult
+    }
+
+    It "Returns a negative result for S or W direction" {
+        # Arrange
+        $degrees = 40
+        $minutes = 20
+        $seconds = 30
+        $direction = 'W'
+
+        # Act
+        $result = Convert-ToDecimalDegrees -degrees $degrees -minutes $minutes -seconds $seconds -direction $direction
+
+        # Assert
+        $expectedResult = -($degrees + ($minutes / 60) + ($seconds / 3600))
+        $result | Should -Be $expectedResult
+    }
+}
