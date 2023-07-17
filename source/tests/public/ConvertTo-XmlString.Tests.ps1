a4cd45269b11fb082f23a1d455877b6fd6f207eb
Christian Niedergesaess <cn@eulanda.de>
2023-07-17 15:22:10 +0200

    Added pester test for ConvertTo-XmlString

---
+++ source/tests/public/ConvertTo-XmlString.Tests.ps1
@@ -0,0 +1,24 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "ConvertTo-XmlString" {
+    It "Should convert a string field to an XML compatible string" {
+        # Arrange
+        $rs = New-Object -ComObject ADODB.Recordset
+        $rs.Fields.Append("MyStringField", $adVarChar, 255) # Size of 255 added for a VarChar field
+        $rs.Open()
+        $rs.AddNew()
+        $rs.Fields("MyStringField").Value = "Test String"
+        $adoField = $rs.Fields("MyStringField")
+
+        # Act
+        $result = ConvertTo-XmlString -adoField $adoField
+
+        # Assert
+        $result | Should -Be "Test String"
+
+        # Cleanup
+        $rs.Close()
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($rs) | Out-Null
+    }
+}
+
