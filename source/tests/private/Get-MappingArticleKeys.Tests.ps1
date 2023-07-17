43c170a3b960f122606e352dc42fb8a431a80455
Christian Niedergesaess <cn@eulanda.de>
2023-07-17 17:39:49 +0200

    Added pester test for Get-MappingArticleKeys

---
+++ source/tests/private/Get-MappingArticleKeys.Tests.ps1
@@ -0,0 +1,19 @@
+# Import the module
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-MappingArticleKeys' {
+    InModuleScope 'EulandaConnect' {
+
+        # Test if the function returns correct hash table
+        It 'should return correct hash table' {
+            $result = Get-MappingArticleKeys
+
+            $result | Should -BeOfType [HashTable]
+            $result.Count | Should -Be 4
+            $result['articleId'] | Should -Be 'Id'
+            $result['articleNo'] | Should -Be 'ArtNummer'
+            $result['articleUid'] | Should -Be 'Uid'
+            $result['Barcode'] | Should -Be 'Barcode'
+        }
+    }
+}
