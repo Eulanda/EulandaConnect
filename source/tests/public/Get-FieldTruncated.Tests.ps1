80cb3febeec61046751b0a195a432e5b1e5804a6
Christian Niedergesaess <cn@eulanda.de>
2023-07-17 15:27:31 +0200

    Added pester test for Get-FieldTruncated

---
+++ source/tests/public/Get-FieldTruncated.Tests.ps1
@@ -0,0 +1,24 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Get-FieldTruncated" {
+    It "Should truncate the field value based on maxLen" {
+        # Arrange
+        $rs = New-Object -ComObject ADODB.Recordset
+        $rs.Fields.Append("MyStringField", $adVarChar, 255)
+        $rs.Open()
+        $rs.AddNew()
+        $rs.Fields("MyStringField").Value = "This is a test string with more than twenty characters."
+        $fieldName = "MyStringField"
+        $maxLen = 20
+
+        # Act
+        $result = Get-FieldTruncated -rs $rs -fieldname $fieldName -maxLen $maxLen
+
+        # Assert
+        $result.Length | Should -BeLessOrEqual $maxLen
+
+        # Cleanup
+        $rs.Close()
+        [System.Runtime.InteropServices.Marshal]::ReleaseComObject($rs) | Out-Null
+    }
+}
