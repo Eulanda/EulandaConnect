9c5051ea3037cd5fea07aa28defc99ebf59fe522
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 18:47:14 +0200

    Added pester test for Test-ValidateNo

---
+++ source/tests/private/Test-ValidateNo.Tests.ps1
@@ -0,0 +1,19 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Test-ValidateNo" {
+    InModuleScope 'EulandaConnect' {
+
+        It "Returns the transaction number when it is valid" {
+            $result = Test-ValidateNo -transactionNo 1234 -name "Transaction Number"
+            $result | Should -Be 1234
+        }
+
+        It "Throws an error when the transaction number is less than 1" {
+            { Test-ValidateNo -transactionNo 0 -name "Transaction Number" } | Should -Throw
+        }
+
+        It "Throws an error when the transaction number is negative" {
+            { Test-ValidateNo -transactionNo -1 -name "Transaction Number" } | Should -Throw
+        }
+    }
+}
