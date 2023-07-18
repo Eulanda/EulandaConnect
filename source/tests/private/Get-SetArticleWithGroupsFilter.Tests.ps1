36d3d6454d9b18ec1fff4ef7e4b6a6cda35a4682
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 05:41:36 +0200

    Added pester test for Get-SetArticleWithGroupsFilter

---
+++ source/tests/private/Get-SetArticleWithGroupsFilter.Tests.ps1
@@ -0,0 +1,21 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-SetArticleWithGroupsFilter' {
+    InModuleScope 'EulandaConnect' {
+
+        # Test if the function returns correct array
+        It 'should return correct array' {
+            $result = Get-SetArticleWithGroupsFilter
+
+            $result.GetType().Name | Should -Be 'Object[]'
+            $result.Count | Should -Be 7
+            $result[0] | Should -Be 'select'
+            $result[1] | Should -Be 'filter'
+            $result[2] | Should -Be 'order'
+            $result[3] | Should -Be 'alias'
+            $result[4] | Should -Be 'reorder'
+            $result[5] | Should -Be 'revers'
+            $result[6] | Should -Be 'customerGroups'
+        }
+    }
+}
