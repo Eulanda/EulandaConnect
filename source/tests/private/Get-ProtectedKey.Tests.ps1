2b049f89be1e94a08b29632fb9a0d2d2dcd27c8b
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 05:07:55 +0200

    Added pester test for Get-ProtectedKey

---
+++ source/tests/private/Get-ProtectedKey.Tests.ps1
@@ -0,0 +1,26 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-ProtectedKey' {
+    InModuleScope 'EulandaConnect' {
+
+        It 'should return byte array when the key length is less than 16' {
+            $result = Get-ProtectedKey -key 'test'
+            $result | ForEach-Object { $_ | Should -BeOfType [Byte] }
+            $result.Length | Should -Be 32
+        }
+
+        It 'should return byte array when the key length is less than 32 but more than 16' {
+            $result = Get-ProtectedKey -key 'ThisIsALongerTestKey'
+            $result | ForEach-Object { $_ | Should -BeOfType [Byte] }
+            $result.Length | Should -Be 32
+        }
+
+        It 'should throw exception when the key length is 32 or more' {
+            { Get-ProtectedKey -key 'ThisIsAnEvenLongerTestKeyThatIsOver32Characters' } | Should -Throw
+        }
+
+        It 'should throw exception when no key is provided' {
+            { Get-ProtectedKey } | Should -Throw
+        }
+    }
+}
