2e38de1625d83986a63fbcdb41b081dfa3f0105c
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 18:13:15 +0200

    Added pester test for Test-ValidateCustomerGroups

---
+++ source/tests/private/Test-ValidateCustomerGroups.Tests.ps1
@@ -0,0 +1,30 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+# Describe the group of tests
+Describe 'Test-ValidateCustomerGroups' {
+    InModuleScope 'EulandaConnect' {
+        # Test with valid input
+        It 'Should not throw an exception with valid customerGroups' {
+            $validCustomerGroups = "Group1,Group2,Group 3"
+            { Test-ValidateCustomerGroups -customerGroups $validCustomerGroups } | Should -Not -Throw
+        }
+
+        # Test with invalid input
+        It 'Should throw an exception with invalid customerGroups' {
+            $invalidCustomerGroups = "Group1,Group2,Group#3"
+            { Test-ValidateCustomerGroups -customerGroups $invalidCustomerGroups } | Should -Throw
+        }
+
+        # Test with empty string
+        It 'Should not throw an exception with an empty string' {
+            $emptyCustomerGroups = ""
+            { Test-ValidateCustomerGroups -customerGroups $emptyCustomerGroups } | Should -Not -Throw
+        }
+
+        # Test with null
+        It 'Should not throw an exception with null' {
+            $nullCustomerGroups = $null
+            { Test-ValidateCustomerGroups -customerGroups $nullCustomerGroups } | Should -Not -Throw
+        }
+    }
+}
---
+++ source/tests/private/Test-ValidateFileExists.Tests.ps1
@@ -0,0 +1,31 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Test-ValidateFileExists' {
+    InModuleScope 'EulandaConnect' {
+
+        BeforeAll {
+            # Create a temporary file in the Temp folder
+            $filePath = Join-Path -Path $env:TEMP -ChildPath "tempFile.txt"
+            Set-Content -Path $filePath -Value "This is a temporary file for testing."
+        }
+
+        It 'should return true when the file exists' {
+            $result = Test-ValidateFileExists -path $filePath
+            $result | Should -Be $true
+        }
+
+        It 'should throw an exception when the file does not exist' {
+            # Create a path to a file that does not exist
+            $nonExistentFilePath = Join-Path -Path $env:TEMP -ChildPath "nonexistentfile.txt"
+
+            { Test-ValidateFileExists -path $nonExistentFilePath } | Should -Throw
+        }
+
+        AfterAll {
+            # Remove the temporary file
+            if (Test-Path $filePath) {
+                Remove-Item -Path $filePath
+            }
+        }
+    }
+}
