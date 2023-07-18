2810a7dceb194eb77f77c1588cda2fcbaa928660
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 18:49:34 +0200

    Added pester test for Test-ValidateNotEmpty

---
+++ source/tests/private/Test-ValidateNotEmpty.Tests.ps1
@@ -0,0 +1,15 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Test-ValidateNotEmpty" {
+    InModuleScope 'EulandaConnect' {
+
+        It "Returns true when the parameter is not empty" {
+            $result = Test-ValidateNotEmpty -strParam "not empty"
+            $result | Should -BeTrue
+        }
+
+        It "Throws an error when the parameter is empty" {
+            { Test-ValidateNotEmpty -strParam "" } | Should -Throw
+        }
+    }
+}
