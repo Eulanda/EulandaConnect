fecde66e30fb0a58349d06a013054a16ff5fb610
Christian Niedergesaess <cn@eulanda.de>
2023-07-18 17:09:25 +0200

    Added pester test for Get-UsedParameters

---
+++ source/tests/private/Get-UsedParameters.Tests.ps1
@@ -0,0 +1,22 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe 'Get-UsedParameters' {
+    InModuleScope 'EulandaConnect' {
+
+        It 'should return the bound parameters that are valid' {
+            # Arrange
+            $validParams = @('param1', 'param2', 'param3')
+            $boundParams = @{ param1 = 'value1'; param2 = 'value2'; param4 = 'value4'}
+
+            # Act
+            $result = Get-UsedParameters -validParams $validParams -boundParams $boundParams
+
+            # Assert
+            $result.Keys.Count | Should -Be 2
+            $result['param1'] | Should -Be 'value1'
+            $result['param2'] | Should -Be 'value2'
+            $result.ContainsKey('param3') | Should -Be $false
+            $result.ContainsKey('param4') | Should -Be $false
+        }
+    }
+}
