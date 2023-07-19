e14f591eaf9c08ee5045703d8d200df112cde7c2
Christian Niedergesaess <cn@eulanda.de>
2023-07-19 13:01:11 +0200

    Changes for Admin in Find-MssqlServer

--- source/tests/public/Find-MssqlServer.Tests.ps1
+++ source/tests/public/Find-MssqlServer.Tests.ps1
@@ -2,24 +2,47 @@
 Import-Module -Name .\EulandaConnect.psd1
 
 Describe 'Find-MssqlServer' {
+
+    BeforeAll {
+        $isAdmin = Test-Administrator
+        if (-not $isAdmin) {
+            Write-Warning "These tests require administrator rights. Run the tests as administrator."
+        }
+    }
+
+
     It 'returns the expected object type when valid IP is provided' {
-        $result = Find-MssqlServer -localIp '127.0.0.1'
-        $result | Should -Not -BeNullOrEmpty
-        $result[0] | Should -BeOfType [PSCustomObject]
+        if ($isAdmin) {
+            $result = Find-MssqlServer -localIp '127.0.0.1'
+            $result | Should -Not -BeNullOrEmpty
+            $result[0] | Should -BeOfType [PSCustomObject]
+        } else {
+            Set-ItResult -Skipped -Because 'Test requires administrator rights'
+            return
+        }
     }
 
     It 'should contain expected properties when valid IP is provided' {
-        $result = Find-MssqlServer -localIp '127.0.0.1'
-        $result[0].Ip.IpAddressToString | Should -Not -BeNullOrEmpty
-        $result[0].InstanceName | Should -Not -BeNullOrEmpty
+        if ($isAdmin) {
+            $result = Find-MssqlServer -localIp '127.0.0.1'
+            $result[0].Ip.IpAddressToString | Should -Not -BeNullOrEmpty
+            $result[0].InstanceName | Should -Not -BeNullOrEmpty
+        } else {
+            Set-ItResult -Skipped -Because 'Test requires administrator rights'
+            return
+        }
     }
 
     It 'should return result within timeout when valid IP is provided' {
-        $timeoutSeconds = 2
-        $startTime = Get-Date
-        $result = Find-MssqlServer -localIp '127.0.0.1' -timeoutSeconds $timeoutSeconds
-        $elapsedTime = (Get-Date) - $startTime
-        $elapsedTime.TotalSeconds | Should -BeLessThan ($timeoutSeconds + 1)  # Allow a little grace period
+        if ($isAdmin) {
+            $timeoutSeconds = 2
+            $startTime = Get-Date
+            $result = Find-MssqlServer -localIp '127.0.0.1' -timeoutSeconds $timeoutSeconds
+            $elapsedTime = (Get-Date) - $startTime
+            $elapsedTime.TotalSeconds | Should -BeLessThan ($timeoutSeconds + 1)  # Allow a little grace period
+        } else {
+            Set-ItResult -Skipped -Because 'Test requires administrator rights'
+            return
+        }
     }
-
 }
