927d7ba7e3bed6c75733e6af7e3bb9466ed3996f
Christian Niedergesaess <cn@eulanda.de>
2023-07-19 08:14:23 +0200

    Added pester test for Get-Conn

---
+++ source/tests/public/Get-Conn.Tests.ps1
@@ -0,0 +1,20 @@
+Import-Module -Name .\EulandaConnect.psm1
+
+Describe "Get-Conn" {
+
+    It "Checks if Get-Conn returns the correct object when given a udl parameter" {
+        $udl = Resolve-Path '.\source\tests\EULANDA_1 Pester.udl'
+        $result = Get-Conn -udl $udl
+        $result.state | Should -Be 1
+    }
+
+    It "Checks if Get-Conn returns the correct object when given a connStr parameter" {
+        $connStr = 'Provider=SQLOLEDB.1;Password=bond007;Persist Security Info=True;User ID=eulanda;Initial Catalog=Eulanda_Pester;Data Source=.\PESTER'
+        $result = Get-Conn -connStr $connStr
+        $result.state | Should -Be 1
+    }
+
+    It "Checks if Get-Conn throws an exception when no parameters are given" {
+        { Get-Conn } | Should -Throw
+    }
+}
