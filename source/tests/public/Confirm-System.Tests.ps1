17d4df80ce02541a615b26b1068def6462f58352
Christian Niedergesaess <cn@eulanda.de>
2023-07-19 07:48:46 +0200

    Added pester test for Confirm-System

---
+++ source/tests/public/Confirm-System.Tests.ps1
@@ -0,0 +1,12 @@
+Import-Module ./EulandaConnect.psm1
+
+Describe 'Confirm-System' {
+
+    It 'Runs without throwing with valid parameters' {
+        { Confirm-System -all } | Should -Not -Throw
+    }
+
+    It 'Runs with throwing without parameters' {
+        { Confirm-System  } | Should -Throw
+    }
+}
