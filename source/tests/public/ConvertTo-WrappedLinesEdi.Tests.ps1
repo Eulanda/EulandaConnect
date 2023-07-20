9639f5d90fd0d481f7d5633637b0f2e22deb086b
Christian Niedergesaess <cn@eulanda.de>
2023-07-20 05:07:20 +0200

    Added pester test for ConvertTo-WrappedLinesEdi

---
+++ source/tests/public/ConvertTo-WrappedLinesEdi.Tests.ps1
@@ -0,0 +1,28 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "ConvertTo-WrappedLinesEdi" {
+
+    It "Test with normal text and width 40" {
+        $text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor lacus sed augue commodo dapibus. Suspendisse potenti.'
+        $width = 40
+        $expectedOutput = 'Lorem ipsum dolor sit amet, consectetur', 'adipiscing elit. Sed porttitor lacus ...'
+        $output = ConvertTo-WrappedLinesEdi -text $text -width $width
+        $output | Should -Be $expectedOutput
+    }
+
+    It "Test with long text and small width" {
+        $text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed porttitor lacus sed augue commodo dapibus. Suspendisse potenti.'
+        $width = 20
+        $expectedOutput = 'Lorem ipsum dolor', 'sit amet, consect...'
+        $output = ConvertTo-WrappedLinesEdi -text $text -width $width
+        $output | Should -Be $expectedOutput
+    }
+
+    It "Test with small text and large width" {
+        $text = 'Lorem ipsum'
+        $width = 80
+        $expectedOutput = 'Lorem ipsum', ''
+        $output = ConvertTo-WrappedLinesEdi -text $text -width $width
+        $output | Should -Be $expectedOutput
+    }
+}
