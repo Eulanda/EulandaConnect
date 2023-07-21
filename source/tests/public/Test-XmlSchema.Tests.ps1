635812c0949ae001e6da7d1dda0383c09600ca68
Christian Niedergesäß <cn@eulanda.de>
2023-07-21 20:01:03 +0200

    Added pester test for Test-XmlSchema

---
+++ source/tests/public/Test-XmlSchema.Tests.ps1
@@ -0,0 +1,60 @@
+Import-Module -Name .\EulandaConnect.psd1
+
+Describe "Test-XmlSchema" {
+    BeforeAll {
+        # Prepare XML and XSD file paths
+        $xmlFile = Join-Path -Path $env:TEMP -ChildPath "simple.xml"
+        $schemaFile = Join-Path -Path $env:TEMP -ChildPath "simple.xsd"
+
+        # XML content
+        $xmlContent = @"
+<EULANDA>
+  <METADATA>
+    <VERSION>5.20</VERSION>
+  </METADATA>
+</EULANDA>
+"@
+
+        # XSD content
+        $xsdContent = @"
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <xs:element name="EULANDA">
+    <xs:complexType>
+      <xs:sequence>
+        <xs:element name="METADATA">
+          <xs:complexType>
+            <xs:sequence>
+              <xs:element type="xs:decimal" name="VERSION"/>
+            </xs:sequence>
+          </xs:complexType>
+        </xs:element>
+      </xs:sequence>
+    </xs:complexType>
+  </xs:element>
+</xs:schema>
+"@
+
+        # Create XML and XSD files
+        Set-Content -Path $xmlFile -Value $xmlContent
+        Set-Content -Path $schemaFile -Value $xsdContent
+    }
+
+    It "returns no error when the XML file matches the schema" {
+        # Call the function and check that no error was thrown
+        { Test-XmlSchema -xmlFile $xmlFile -schemaFile $schemaFile } | Should -Not -Throw
+    }
+
+    It "throws an error when the XML file does not match the schema" {
+        # Modify the XML file to make it invalid
+        (Get-Content -Path $xmlFile) -replace "5.20", "invalid" | Set-Content -Path $xmlFile
+
+        # Call the function and check that an error was thrown
+        { Test-XmlSchema -xmlFile $xmlFile -schemaFile $schemaFile } | Should -Throw
+    }
+
+    AfterAll {
+        # Remove the XML and XSD files
+        Remove-Item -Path $xmlFile -ErrorAction SilentlyContinue
+        Remove-Item -Path $schemaFile -ErrorAction SilentlyContinue
+    }
+}
