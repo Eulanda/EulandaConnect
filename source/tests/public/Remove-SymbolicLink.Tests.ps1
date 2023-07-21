6605db9720fb5dbd48d3be2511ba83684ce2881c
Christian Niedergesäß <cn@eulanda.de>
2023-07-21 21:10:01 +0200

    Added pester test for Show-MsgBox und Show-MsgBoxYes

---
+++ source/tests/public/Remove-SymbolicLink.Tests.ps1
@@ -0,0 +1,42 @@
+Import-Module ./EulandaConnect.psm1
+
+InModuleScope EulandaConnect {
+    Describe "Remove-SymbolicLink" {
+        It "removes symbolic link when run as administrator and link exists" {
+            Mock Get-ResStr { return [string]$args[0] }
+            Mock Test-Administrator { return $true }
+            Mock Test-Path { return $true }
+            Mock Get-Item { return @{
+                Attributes = [System.IO.FileAttributes]::ReparsePoint
+            }}
+            Mock Remove-Item {}
+            { Remove-SymbolicLink -symbolicLink 'C:\MySymbolicLink' } | Should -Not -Throw
+            Assert-MockCalled -CommandName 'Remove-Item' -Times 1 -Exactly
+        }
+
+        It "throws when not run as administrator" {
+            Mock Get-ResStr { return [string]$args[1] }
+            Mock Test-Administrator { return $false }
+            Mock Test-Path { return $true }
+            Mock Get-Item { return @{
+                Attributes = [System.IO.FileAttributes]::ReparsePoint
+            }}
+            Mock Remove-Item {}
+            { Remove-SymbolicLink -symbolicLink 'C:\MySymbolicLink' } | Should -Throw -ExpectedMessage "ADMIN_RIGHTS_NEEDED"
+            Assert-MockCalled -CommandName 'Remove-Item' -Times 0
+        }
+
+
+        It "does nothing when symbolic link does not exist" {
+            Mock Get-ResStr { return [string]$args[0] }
+            Mock Test-Administrator { return $true }
+            Mock Test-Path { return $false }
+            Mock Get-Item { return @{
+                Attributes = [System.IO.FileAttributes]::ReparsePoint
+            }}
+            Mock Remove-Item {}
+            { Remove-SymbolicLink -symbolicLink 'C:\MySymbolicLink' } | Should -Not -Throw
+            Assert-MockCalled -CommandName 'Remove-Item' -Times 0
+        }
+    }
+}
