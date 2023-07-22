a3f23bb9432699ac45e7f233a958a4b8575b60c7
Christian Niedergesäß <cn@eulanda.de>
2023-07-22 19:38:48 +0200

    Added pester test for Get-ConnStr

---
+++ source/tests/public/Get-ConnStr.Tests.ps1
@@ -0,0 +1,23 @@
+Import-Module -Name .\EulandaConnect.psd1
+Set-StrictMode -version latest
+
+Describe 'Get-ConnStr' -Tag 'integration' {
+
+    BeforeAll {
+        $udl = Resolve-Path '.\source\tests\EULANDA_1 Pester.udl'
+    }
+
+    It "Returns a connection string when provided UDL and database" {
+        # Arrange
+        $database = "Eulanda_PesterTest"
+
+        # Act
+        $result = Get-ConnStr -udl $udl -database $database
+
+        # Assert
+        $result | Should -Match "Provider=SQLOLEDB.1"
+        $result | Should -Match "Initial Catalog=$database"
+        $result | Should -Match "Persist Security Info=True"
+        $result | Should -Match "User ID=eulanda"
+    }
+}
