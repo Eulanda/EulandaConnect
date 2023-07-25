126a79258460f246b766fcee22b59a507c2b815a
Christian Niedergesäß <cn@eulanda.de>
2023-07-25 14:51:44 +0200

    Added function Get-XmlEulandaArticle.Tests.ps1

---
+++ source/tests/public/Get-XmlEulandaArticle.Tests.ps1
@@ -0,0 +1,94 @@
+Import-Module -Name .\EulandaConnect.psd1
+Set-StrictMode -version latest
+
+Describe 'Get-XmlEulandaArticle' -Tag 'integration', 'sql', 'sqladmin', 'eulanda' {
+
+    BeforeAll {
+        $udl = Resolve-Path ".\source\tests\Eulanda_1 Pester.udl"
+        $articleNo = '4711'
+        $barcode = '1234567890123'
+        $packingUnit = 12
+
+        # Test-MssqlAdministartor rights
+        $skipTest = -not (Test-MssqlAdministrator -udl $udl)
+
+        # Skip backup because for restoring you need special rights
+        if (! $skipTest) {
+            Backup-MssqlDatabase -udl $udl
+
+            # Insert the necessary data into the database
+            $conn = Get-Conn -udl $udl
+            $sql = "INSERT INTO Artikel (ArtNummer, Barcode, Vk, VerpackEH, Kurztext1) VALUES ($articleNo, $barcode, 42.50, $packingUnit, 'Some Info')"
+            $conn.Execute($sql)
+            $conn.close()
+        }
+    }
+
+
+    AfterAll {
+        # Skip restore because for restoring you need special rights
+        if (! $skipTest) {
+            Restore-MssqlDatabase -udl $udl
+        }
+    }
+
+
+    It 'Get all articles as xml string' {
+        if ($skipTest) {
+            Set-ItResult -Skipped -Because 'This test should be skipped due to user not in sysadmin role'
+            Return
+        }
+
+        [string]$xmlStr = Get-XmlEulandaArticle -udl $udl
+        [xml]$xml = $xmlStr
+        $xml.ARTIKELLISTE.ARTIKEL.ARTNUMMER | should -Be $articleNo
+    }
+
+
+    It 'Handle filter with no result' {
+        if ($skipTest) {
+            Set-ItResult -Skipped -Because 'This test should be skipped due to user not in sysadmin role'
+            Return
+        }
+
+        [string]$xmlStr = Get-XmlEulandaArticle -udl $udl -filter "ArtNummer = '0815'"
+        $xmlStr | should -BeNullOrEmpty
+    }
+
+
+    It 'Handles connection errors and missing UDL file appropriately' {
+        if ($skipTest) {
+            Set-ItResult -Skipped -Because 'This test should be skipped due to user not in sysadmin role'
+            Return
+        }
+
+        $nonExistentUdl = 'C:\path\to\nonexistent.udl'
+        { Get-XmlEulandaArticle -udl $nonExistentUdl } | Should -Throw
+    }
+
+
+    It 'Handles invalid connection object' {
+        if ($skipTest) {
+            Set-ItResult -Skipped -Because 'This test should be skipped due to user not in sysadmin role'
+            Return
+        }
+
+        $invalidConn = 'invalid connection'
+        { Get-XmlEulandaArticle -conn $invalidConn } | Should -Throw
+    }
+
+
+    It 'Handles closed database connection appropriately' {
+        if ($skipTest) {
+            Set-ItResult -Skipped -Because 'This test should be skipped due to user not in sysadmin role'
+            Return
+        }
+
+        $closedConn = Get-Conn -udl $udl
+        $closedConn.close() # close it
+        $xmlStr = Get-XmlEulandaArticle -conn $closedConn
+        [xml]$xml = $xmlStr
+        $xml.ARTIKELLISTE.ARTIKEL.ARTNUMMER | should -Be $articleNo
+    }
+
+}
